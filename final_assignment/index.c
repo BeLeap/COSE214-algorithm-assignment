c81d8a38fae19e0a9b0ab39761583fb53409b70d
BeLeap <changseo_jang@korea.ac.kr>
2020-12-14 19:36:05 +0000

    argument count check

--- final_assignment/index.c
+++ final_assignment/index.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 
-int main(void) {}
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+void PrintError(char err[]) {
+  // clang-format off
+  fprintf(stderr,
+          ANSI_COLOR_RED "[!] " ANSI_COLOR_YELLOW "Error: %s\n" ANSI_COLOR_RESET,
+          err);
+  // clang-format on
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    PrintError("Not enough arguments");
+    return 1;
+  }
+}
