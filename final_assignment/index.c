7d3fd50300b59e30e899d679eb54d0f412d3cd60
BeLeap <changseo_jang@korea.ac.kr>
2020-12-14 20:13:34 +0000

    declare destroy functions

--- final_assignment/index.c
+++ final_assignment/index.c
@@ -22,22 +22,63 @@ void Println(char content[]) { printf("%s\n", content); }
 /* Library End */
 /////////////////
 
+// Word List
 typedef struct __Word {
   char* word;
   struct __Word* next;
 } Word;
 
+// Index List
 typedef struct __Index {
   char id[3];
   Word* head;
   struct __Index* next;
 } Index;
 
+// Create and Initialize Index
 Index CreateIndex() {
   Index root;
   memcpy(root.id, "\0\0\0", sizeof(root.id));
   root.head = NULL;
   root.next = NULL;
+}
+
+// Append new word list
+void AppendIndex(Index* root, char id[3], Word* wordRoot) {
+  Index* curr = root;
+  while (curr->next != NULL) {
+    curr = curr->next;
+  }
+
+  Index* newIndex = (Index*)malloc(sizeof(Index));
+  strcpy(newIndex->id, id);
+  newIndex->head = wordRoot;
+  newIndex->next = NULL;
+
+  curr->next = newIndex;
+
+  return;
+}
+
+void DestroyWordList(Word* root) {
+  if (root == NULL) {
+    return;
+  }
+
+  free(root->word);
+  DestroyWordList(root->next);
+
+  return;
+}
+
+void DestroyAll(Index* root) {
+  if (root == NULL) {
+    return;
+  }
+
+  DestroyWordList(root->head);
+  DestroyAll(root->next);
+  return;
 }
 
 int main(int argc, char* argv[]) {
